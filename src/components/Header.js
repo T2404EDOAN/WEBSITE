9be1d5025628e0e0f7e8818b22ed18d547aa156e
fptaptecht2404e@gmail.com <Abc@5555511111>
2024-09-15 10:00:26 +0700

    test

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function header(){
+    <div>
+        <div><a>logo</a></div>
+
+    <div>
+<a>home</a>
+<a>abount</a>
+<a>products</a>
+<a>Lien he</a>
+
+    </div>
+
+
+
+
+    </div>
+}

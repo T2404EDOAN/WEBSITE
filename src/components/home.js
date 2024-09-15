44716ce0a5279b836cfbf9ee3bbf07832fd25072
fptaptecht2404e@gmail.com <Abc@5555511111>
2024-09-15 10:42:11 +0700

    add home

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function home(){
+    <div>
+        <div><a>logo</a></div>
+
+    <div>
+<a>home</a>
+<a>abount</a>
+<a>products</a>
+<a>Lien he</a>
+
+    </div>
+
+
+
+
+    </div>
+}
